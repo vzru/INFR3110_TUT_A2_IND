200c1905541ecd20d726ef8517d6fc03b42a8274
Victor <ru.zhang1@ontariotechu.net>
2019-11-06 16:37:39 -0500

    Initial Commit

---
+++ Assets/_Scripts/BulletController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Util;
+
+public class BulletController : MonoBehaviour
+{
+    public float bulletSpeed = 0.1f;
+    public Boundary boundary;
+
+    //TODO: create a reference to the BulletPoolManager
+    //public BulletPoolManager bulletPoolManager;
+
+    void Start()
+    {
+        boundary.Top = 2.45f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        CheckBounds();
+    }
+
+    private void Move()
+    {
+        transform.position += new Vector3(0.0f, bulletSpeed, 0.0f);
+    }
+
+    private void CheckBounds()
+    {
+        if (transform.position.y >= boundary.Top)
+        {
+            //TODO: This code needs to change to use the BulletPoolManager's
+            //TODO: ResetBullet function which will return the bullet to the pool
+            BulletPoolManager.GetInstance().ResetBullet(this.gameObject);
+        }
+    }
+}
---
+++ Assets/_Scripts/BulletPoolManager.cs
@@ -0,0 +1,173 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+// TODO: Bonus - make this class a Singleton!
+
+//[System.Serializable]
+public class BulletPoolManager : MonoBehaviour
+{
+    private static BulletPoolManager _instance;
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    public static BulletPoolManager GetInstance()
+    {
+        return _instance;
+    }
+    public GameObject bullet;
+    public int type = 0;
+
+
+    //TODO: create a structure to contain a collection of bullets
+    public List<GameObject> bulletPool;
+    public List<GameObject> activeBullets;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // TODO: add a series of bullets to the Bullet Pool
+        for (int i = 0; i < 20; i++)
+        {
+            bulletPool.Add(CreateInstance(type));
+        }
+
+        foreach (GameObject Bullet in bulletPool)
+        {
+            Bullet.gameObject.SetActive(false);
+            Bullet.transform.parent = this.transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            type = 0;
+            if (bulletPool.Count == 20)
+            {
+                for (int i = bulletPool.Count - 1; i >= 0; i--)
+                {
+                    Destroy(bulletPool[i]);
+                    //bulletPool.RemoveAt(i);
+                }
+                bulletPool.Clear();
+                for (int i = 0; i < 20; i++)
+                {
+                    bulletPool.Add(CreateInstance(type));
+                }
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            type = 1;
+            if (bulletPool.Count == 20)
+            {
+                for (int i = bulletPool.Count - 1; i >= 0; i--)
+                {
+                    Destroy(bulletPool[i]);
+                    //bulletPool.RemoveAt(i);
+                }
+                bulletPool.Clear();
+                for (int i = 0; i < 20; i++)
+                {
+                    bulletPool.Add(CreateInstance(type));
+                }
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            type = 2;
+            if (bulletPool.Count == 20)
+            {
+                for (int i = bulletPool.Count - 1; i >= 0; i--)
+                {
+                    Destroy(bulletPool[i]);
+                    //bulletPool.RemoveAt(i);
+                }
+                bulletPool.Clear();
+                for (int i = 0; i < 20; i++)
+                {
+                    bulletPool.Add(CreateInstance(type));
+                }
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            type = 3;
+            if (bulletPool.Count == 20)
+            {
+                for (int i = bulletPool.Count - 1; i >= 0; i--)
+                {
+                    Destroy(bulletPool[i]);
+                    //bulletPool.RemoveAt(i);
+                }
+                bulletPool.Clear();
+                for (int i = 0; i < 20; i++)
+                {
+                    bulletPool.Add(CreateInstance(type));
+                }
+            }
+        }
+    }
+
+    public GameObject CreateInstance(int type)
+    {
+        GameObject newInstance = MonoBehaviour.Instantiate(bullet, Vector3.zero, Quaternion.identity);
+
+        Renderer renderer = newInstance.GetComponent<Renderer>();
+        switch (type)
+        {
+            case 0:
+                break;
+            case 1:
+                renderer.material.SetColor("_Color", new Color(1.0f, 0.0f, 0.0f, 1.0f));
+                break;
+            case 2:
+                renderer.material.SetColor("_Color", new Color(0.0f, 1.0f, 0.0f, 1.0f));
+                break;
+            case 3:
+                renderer.material.SetColor("_Color", new Color(0.0f, 0.0f, 1.0f, 1.0f));
+                break;
+            default:
+                break;
+        }
+
+        return newInstance;
+    }
+
+    //TODO: modify this function to return a bullet from the Pool
+    public GameObject GetBullet(Vector3 pos)
+    {
+        activeBullets.Add(bulletPool[bulletPool.Count - 1].gameObject);
+        GameObject temp = activeBullets[activeBullets.Count - 1].gameObject;
+        temp.gameObject.SetActive(true);
+        temp.transform.position = pos;
+
+        bulletPool.RemoveAt(bulletPool.Count - 1);
+
+        return temp;
+    }
+
+    //TODO: modify this function to reset/return a bullet back to the Pool 
+    public void ResetBullet(GameObject bullet)
+    {
+        bulletPool.Add(bullet);
+        bullet.gameObject.SetActive(false);
+
+        activeBullets.Remove(bullet);
+    }
+}
